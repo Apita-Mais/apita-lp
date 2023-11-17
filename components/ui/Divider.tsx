90d61331d41b76878a24b331a8c7ae7453e01b12
guhrodriguess <gumartinsR1201@gmail.com>
2023-11-16 22:34:51 -0300

    refactor: Removing unused code

--- components/ui/Divider.tsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function Divider({ className }: { className?: string }) {
-  return (
-    <div
-      className={cn("mx-4 block h-[25px] w-px bg-foreground/30", className)}
-    />
-  );
-}
